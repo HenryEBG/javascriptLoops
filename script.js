f2024bfc15ef22d76ff5c86bedf82b46a036600f
HenryEBG <berganza386@gmail.com>
2024-04-13 20:50:10 -0400

    Adding internal documentation

--- script.js
+++ script.js
@@ -83,41 +83,49 @@ else {
 
 // Print a title for the exercise
 console.log("***************************")
-console.log("*** Prime Time Exercise ***")
+console.log("****** Feeling Loopy ******")
 console.log("***************************")
 
+//create temporal string to get one line of the 
 let rowString=""
+//array to save the different cells every line can have
 let cells=[]
 
+//initial string with all the data
 let dataString ="Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232"
 
-//console.log(dataString)
 
+//While loop that separate the initial string in lines
  while(dataString.length>0){
   //buscar \n
-  //si encuentra la cadena tomamos todo hasta antes de la \n
+  //If find the  \n symbol takes the line
   if(dataString.indexOf("\n")!==-1){
     rowString=dataString.slice(0,dataString.indexOf("\n"))
     dataString=dataString.slice(dataString.indexOf("\n")+1,dataString.length)
+    //if not find the \n symbol means that is the last line and the initial string go to 0 lenght
   } else {
     rowString=dataString.slice(0,dataString.length)
     dataString=""
   }
 
+  //variable that it will be the index of our array
   let index=0;
+  //while loop that separate the line in cells
    while(rowString.length>0||index===3){
+      //if find a , it means that is a cell
      if(rowString.indexOf(",")!==-1){
       cells[index]=rowString.slice(0,rowString.indexOf(","))
       rowString=rowString.slice(rowString.indexOf(",")+1,rowString.length)
+      //if don't find the , means is the last cell and the line get 0 lenght to get out of the loop
    } else {
       cells[index]=rowString
       rowString=""
    }
+   //increment the index
    index++
   }
-
+  //print every line separate in cells (in this case 4 cells from 0 to 3)
    console.log(`${cells[0]},${cells[1]},${cells[2]},${cells[3]}`)
+   //we iniciate the cells to 0 lenght to get another line.
    cells.length=0
-
-
  }
